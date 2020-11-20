352f12de003305bb3c4a6a84fc90026b9c067dc1
meter CJY <854916013@qq.com>
2020-11-20 17:23:59 +0800

    2020年11月20日17:23:57

--- common/api.js
+++ common/api.js
@@ -2,12 +2,12 @@ import {request,common} from './request.js'
 
 // 获取地理位置 经纬度
 export function geolocate() { // 个人信息 get
-    return request(`https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAegNeT3FywkU9a4_IaJF0omXOILWbZfws`,'POST')
+    return request(`https://www.googleapis.com/geolocation/v1/geolocate?key=key`,'POST')
 }
 // 获取具体位置 国家省市区
 export function geocode(query) { // 个人信息 get
     return request(
-        `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAegNeT3FywkU9a4_IaJF0omXOILWbZfws`,
+        `https://maps.googleapis.com/maps/api/geocode/json?key=key`,
         'POST',
         query
     )
